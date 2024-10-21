babe669e923b0fdf75917ebb7b7a2744315f2b1f
Mattias Ulmestrand <mattias@taigatech.se>
2024-10-21 22:30:20 +0200

    General improvements

--- colormaps.py
+++ colormaps.py
@@ -8,33 +8,38 @@ from color_utils import *
 from argparse import ArgumentParser
 
 
-def get_continuous_cmap(cmap_name: str, n: int, ijk: Tuple | None = None):
+def get_colormap(
+        cmap_name: str, 
+        n: int, 
+        ijk: Tuple | None = None, 
+        lightness: str | None = None
+    ):
     space = np.linspace(0, 1, n)
 
-    if cmap_name.endswith(".json"):
+    if not cmap_name in CMAP_DICT.keys():
         json_data = load_json(cmap_name, n)
         control_points = np.array(json_data["points"])
-        vals = interpolate_lab(control_points, n, json_data["profile"])
-        vals, _, _ = rgb_renormalized_lightness(vals)
+
+        if lightness is None:
+            lightness = json_data["lightness"]
+
+        interpolated_values = interpolate_lab(control_points, n, lightness)
+        rgb_values, _, _ = rgb_renormalized_lightness(interpolated_values, lightness)
     else:
-        vals = CMAP_DICT[cmap_name](space, ijk)
+        rgb_values = CMAP_DICT[cmap_name](space, ijk)
     
     cdict = dict()
 
     for num, col in enumerate(["red", "green", "blue"]):
-        col_list = [[space[i], vals[i][num], vals[i][num]] for i in range(n)]
+        col_list = [[space[i], rgb_values[i][num], rgb_values[i][num]] for i in range(n)]
         cdict[col] = col_list
     cmp = mcolors.LinearSegmentedColormap(cmap_name, segmentdata=cdict, N=n)
     return cmp
 
 
-def get_custom_cmap(name: str = "cold_blood", n: int = 1000, ijk: tuple = None):
-    cmp = get_continuous_cmap(name, n, ijk)
-    return cmp
-
-
 CMAP_DICT = {
     "cold_blood": cold_blood,
+    "ectotherm": cold_blood,
     "copper_salt": copper_salt
 }
 
@@ -55,13 +60,21 @@ def parse_args():
         default=1000, 
         help="Number of points in the colormap spectrum"
     )
+    arg_parser.add_argument(
+        "--lightness", 
+        "-l", 
+        type=str, 
+        default=None, 
+        help="The lightness profile of the colormap. Allowed values are: " + 
+             "linear, diverging, diverging_inverted, flat."
+    )
     return arg_parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
     n = args.num_points
-    cmap = get_custom_cmap(args.colormap, n)
+    cmap = get_colormap(args.colormap, n, lightness=args.lightness)
     # cmap = colormaps["cividis"]
 
     gradient = np.linspace(0, 1, n)
@@ -76,40 +89,41 @@ if __name__ == "__main__":
     gradient_gray = np.vstack((lightness, lightness))
 
     # Plotting
-    fig, axes = plt.subplots(3, 1, figsize=(10, 10), constrained_layout=True)
+    fig = plt.figure(figsize=(20, 10), constrained_layout=True)
+    gs = fig.add_gridspec(2, 2)
 
-    # Plot RGB gradient
-    axes[0].imshow(gradient_rgb, aspect='auto', vmin=0.0, vmax=1.0)
-    axes[0].set_title('RGB gradient')
-    axes[0].axis('off')
+    # Define the axes
+    ax0 = fig.add_subplot(gs[0, 0])  # Top-left subplot
+    ax1 = fig.add_subplot(gs[1, 0])  # Bottom-left subplot
+    ax2 = fig.add_subplot(gs[:, 1])  # Right subplot spanning both rows
 
-    # Plot Grayscale gradient
-    axes[1].imshow(gradient_gray, cmap='gray', aspect='auto', vmin=0.0, vmax=1.0)
-    axes[1].set_title('Lightness gradient')
-    axes[1].axis('off')
+    # Plot RGB gradient on ax0
+    ax0.imshow(gradient_rgb, aspect='auto', vmin=0.0, vmax=1.0)
+    ax0.set_title('RGB gradient', fontsize=20)
+    ax0.axis('off')
 
+    # Plot Grayscale gradient on ax1
+    ax1.imshow(gradient_gray, cmap='gray', aspect='auto', vmin=0.0, vmax=1.0)
+    ax1.set_title('Lightness gradient', fontsize=20)
+    ax1.axis('off')
+
+    # Plot the color channels and luminance on ax2
     for c, color_string in zip(range(3), ["red", "green", "blue"]):
-        axes[2].plot(gradient_rgb[0, :, c], color=color_string, label=color_string)
-    
-    axes[2].plot(lightness, color="grey", linestyle="--", label="lightness")
-    axes[2].plot(luminance, color="black", label="luminance")
-    plt.legend()
+        ax2.plot(gradient_rgb[0, :, c], color=color_string, label=color_string, linewidth=4)
 
+    ax2.plot(lightness, color="grey", linestyle="--", label="lightness", linewidth=4)
+    ax2.plot(luminance, color="black", label="luminance")
+    ax2.set_title("Color channel intensities", fontsize=20)
+    ax2.legend(loc=0, fontsize=20)
     plt.show()
 
-    for c, color_string in zip(range(3), ["red", "green", "blue"]):
-        plt.plot(gradient_rgb[0, :, c], color=color_string, label=color_string)
-    
-    plt.plot(lightness, color="grey", linestyle="--", label="lightness")
-    plt.plot(luminance, color="black", label="luminance")
-    plt.legend()
-    plt.show()
-
-    dem_data = plt.imread("/home/mattias/imgs/DEM3.gif")
+    test_image = Path(__file__).parent / "test_images" / "DEM3.gif"
+    dem_data = plt.imread(test_image)
     
     if len(dem_data.shape) == 3:
         dem_data = dem_data[:, :, 0]
         
     plt.imshow(dem_data, cmap=cmap)
+    plt.title("Example digital elevation data", fontsize=20)
     plt.colorbar()
     plt.show()
