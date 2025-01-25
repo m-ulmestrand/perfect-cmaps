970e863c7dd8b47e7e6b9214f8870a99d3654840
Mattias <m.ulmestrand@gmail.com>
2025-01-25 19:33:36 +0100

    Add build files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="perfect_cmaps",
+    version="1.0.0",
+    author="Mattias Ulmestrand",
+    author_email="m.ulmestrand@gmail.com",
+    description="Create amazing perceptually uniform colormaps with ease!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/m-ulmestrand/perfect-cmaps",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License, Version 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy",
+        "numba",
+        "pandas",
+        "matplotlib",
+        "colour-science",
+        "scipy",
+        "opencv-python-headless",
+        "importlib_resources",
+    ],
+)
