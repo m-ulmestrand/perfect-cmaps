e28760e205ff36d4278a72ee06f29345a95d5d15
Mattias Ulmestrand <m.ulmestrand@gmail.com>
2026-07-11 21:31:45 +0200

    Add Cython kernel for optimization

--- setup.py
+++ setup.py
@@ -1,39 +1,26 @@
-from setuptools import setup, find_packages
-import re
+import os
+from setuptools import setup, Extension
+import numpy as np
 
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
 
-# Read the version directly from __init__.py
-with open("perfect_cmaps/__init__.py", "r") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+ext = ".pyx" if USE_CYTHON else ".c"
 
+extensions = [
+    Extension(
+        "perfect_cmaps._optimization",
+        [os.path.join("perfect_cmaps", "_optimization" + ext)],
+        include_dirs=[np.get_include()],
+    )
+]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions, compiler_directives={"language_level": "3"})
 
 setup(
-    name="perfect_cmaps",
-    version=version,
-    author="Mattias Ulmestrand",
-    author_email="m.ulmestrand@gmail.com",
-    description="Create amazing perceptually uniform colormaps with ease!",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/m-ulmestrand/perfect-cmaps",
-    packages=find_packages(),
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-        "numpy",
-        "numba",
-        "pandas",
-        "matplotlib",
-        "colour-science",
-        "scipy",
-        "opencv-python-headless",
-        "importlib_resources",
-        "appdirs",
-        "tqdm"
-    ],
+    ext_modules=extensions,
 )
